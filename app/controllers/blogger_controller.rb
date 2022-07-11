1690f244ab48cf5dc54c201d78fe1af198f2c3cd
shekhar <official.shekhar@hotmail.com>
2022-07-11 15:45:48 +0530

    CRUD operation Completed

--- app/controllers/blogger_controller.rb
+++ app/controllers/blogger_controller.rb
@@ -8,4 +8,52 @@ class BloggerController<ApplicationController
     def show
          @blog = BlogDb.find(params[:id])
     end
+
+    def new
+          @blog=BlogDb.new()
+    end
+
+    def create
+     @blog = BlogDb.new({:name=>params[:name],:article=>params[:article]})
+     # @blog.name=params[:name]
+     # @blog.article=params[:article]
+          if @blog.save
+          redirect_to blogger_url(@blog[:id])
+          else
+          render :new, status: :unprocessable_entity
+          end
+     end
+
+     def edit
+          @blog=BlogDb.find(params[:id])
+     end
+
+     def update
+          @blog = BlogDb.find(params[:id])
+      
+          if @blog.update({:name=>params[:name],:article=>params[:article]})
+            redirect_to blogger_url(@blog[:id])
+          else
+            render :edit, status: :unprocessable_entity
+          end
+     end  
+
+     def destroy
+          @blog = BlogDb.find(params[:id])
+          @blog.destroy
+          redirect_to blogger_index_path, status: :see_other
+     end
+          
+
+
+
+
+
+
+
+
+#      private
+#     def article_params
+#       params.require(:article).permit(:title, :body)
+#     end
 end
